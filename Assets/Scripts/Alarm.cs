68b948dc841a522f9762aaccf4678d6102a4c0f9
Prog84 <bvv198424@rambler.ru>
2020-09-28 12:56:09 +1000

    Initial commit

---
+++ Assets/Scripts/Alarm.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Alarm : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private float _duration;
+    private Coroutine _coroutine;
+    private bool _isAlarm = false;   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Movement>(out Movement movement))
+        {
+            if (!_isAlarm && movement.Direction.x > 0)
+            {
+                _isAlarm = true;
+                StartAlarm(_isAlarm);
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Movement>(out Movement movement))
+        {
+            if (_isAlarm && movement.Direction.x < 0)
+            {
+                _isAlarm = false;
+                StartAlarm(_isAlarm);
+            }
+        }
+    }
+    private void StartAlarm(bool isAlarm)
+    {
+        if (_coroutine == null)
+        {
+            _coroutine = StartCoroutine(SoundVolumeAlarm(_duration, isAlarm));
+        }
+        else
+        {
+            StopCoroutine(_coroutine);
+            _coroutine = StartCoroutine(SoundVolumeAlarm(_duration, isAlarm));
+        }    
+    }
+    private IEnumerator SoundVolumeAlarm(float duration, bool isAlarm)
+    {
+        float currentTime = 0;
+        float startSound;
+        float endSound;
+        if (isAlarm)
+        {
+            startSound = 0;
+            endSound = 1;
+        }
+        else
+        {
+            startSound = 1;
+            endSound = 0;
+        }
+        _audioSource.volume = startSound;
+        _audioSource.Play();
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            _audioSource.volume = Mathf.Lerp(startSound, endSound, currentTime / duration);
+            yield return null;
+        }
+        _coroutine = null;
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private Rigidbody2D _rigidbodyPlayer;
+    [SerializeField] private Vector3 _direction;
+    public Vector3 Direction => _direction;
+
+    private void Update()
+    {
+        Move();
+        _animator.SetFloat("Speed", Mathf.Abs(_direction.x));
+    }
+
+    private void Move()
+    {
+        _direction = Vector3.zero;
+        if (Input.GetKey(KeyCode.A))
+            _direction = Vector3.left;
+        if (Input.GetKey(KeyCode.D))
+            _direction = Vector3.right;
+        _direction *= _speed;
+        _rigidbodyPlayer.velocity = _direction;
+        if (_direction.x > 0)
+            _spriteRenderer.flipX = false;
+        if (_direction.x < 0)
+            _spriteRenderer.flipX = true;
+    }
+}
